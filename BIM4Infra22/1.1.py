2af2945167806bb559f7ddf42da83b85951deeeb
davidvlaminck <davidvlaminck85@gmail.com>
2022-11-14 14:31:13 +0100

    1.1 example modified

--- BIM4Infra22/1.1.py
+++ BIM4Infra22/1.1.py
@@ -8,6 +8,5 @@ if __name__ == "__main__":
     kast.toestand = 'in-gebruik'
     kast.naam = 'A0013.K'
     kast.heeftMaaibescherming = True
-    # kast.heeftMaaibescherming = 20 # Deze lijn geeft een foutmelding wanneer je het eerste # teken weghaalt
-    # kast.heeftMaaibescherming = 'True' # Deze lijn geeft een waarschuwing wanneer je het eerste # teken weghaalt
 
+    print(kast)
