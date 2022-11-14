aa6ce867c9abaf13d05358192cfff403c2e19797
davidvlaminck <davidvlaminck85@gmail.com>
2022-11-14 10:59:41 +0100

    1.1 example added

--- BIM4Infra22/1.1.py
+++ BIM4Infra22/1.1.py
@@ -1,1 +1,13 @@
+# pip install otlmow_model
+
+from otlmow_model.Classes.Onderdeel.Wegkantkast import Wegkantkast
+
+if __name__ == "__main__":
+    kast = Wegkantkast()
+    kast.isActief = True
+    kast.toestand = 'in-gebruik'
+    kast.naam = 'A0013.K'
+    kast.heeftMaaibescherming = True
+    # kast.heeftMaaibescherming = 20 # Deze lijn geeft een foutmelding wanneer je het eerste # teken weghaalt
+    # kast.heeftMaaibescherming = 'True' # Deze lijn geeft een waarschuwing wanneer je het eerste # teken weghaalt
 
