1cfa9e81a2cebfdab2e20c622b475678a99e0352
davidvlaminck <davidvlaminck85@gmail.com>
2022-11-18 15:52:00 +0100

    code for template added

--- BIM4Infra22/4.1.py
+++ BIM4Infra22/4.1.py
@@ -3,10 +3,15 @@
 import logging
 from pathlib import Path
 
+from otlmow_template.SubsetTemplateCreator import SubsetTemplateCreator
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(message)s')
 
-    subset_tool = SubsetTool()
-    subset_location = Path('./OTL_AllCasesTestClass.db')
-    csv_location = Path('./template_file_text.csv')
+    template_creator = SubsetTemplateCreator()
+    subset_location = Path('./camera_steun.db')
+    csv_location = Path('./template_file.csv')
+
+    template_creator.generate_template_from_subset(path_to_subset=subset_location,
+                                                   path_to_template_file_and_extension=csv_location,
+                                                   split_per_type=True)
